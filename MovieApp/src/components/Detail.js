cce97775d0c3ee25a44f406c533cafa57670e9c6
nareun130 <nareun130@gmail.com>
2023-01-25 20:50:55 +0900

    영화 앱
    nomad coder의 css 추가

--- MovieApp/src/components/Detail.js
+++ MovieApp/src/components/Detail.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import styles from "./Movie.module.css"
 function Detail() {
   const { id } = useParams(); //ReactRoute가 id값을 찾아오는 곳
   const [loading, setLoading] = useState(true);
@@ -23,17 +23,19 @@ function Detail() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
-          <img src={movie.medium_cover_image} alt={movie.title} />
-          <h1>{movie.title}</h1>
+        <div className={styles.movie}>
+          <img src={movie.medium_cover_image} alt={movie.title} className={styles.movie__img}/>
+          <div>
+          <h1 className={styles.movie__title}>{movie.title}</h1>
           <p>{movie.description_intro}</p>
           <b>genres</b>
           <br />
-          <ul>
+          <ul className={styles.movie__genres}>
             {movie.genres.map((g) => (
               <li key={g}>{g}</li>
             ))}
           </ul>
+          </div>
         </div>
       )}
     </div>
